0e7b1bcd52d75a0f34215de932f497bd04f1a5ee
Oleh Patlan <afftar.nash1@gmail.com>
2019-11-29 21:47:45 +0200

    Adding new class

---
+++ src/net/oleja/NewClass.java
@@ -0,0 +1,18 @@
+package net.oleja;
+
+public class NewClass {
+    private  int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
